637aab3e97700e8addfc9bac821734719700f8e6
devanbenz <devandbenz@gmail.com>
2025-04-07 16:02:23 -0500

    feat: initial C bindings test

--- build.rs
+++ build.rs
@@ -2,18 +2,22 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    println!("cargo:rustc-link-search=/leanstore/backend/leanstore");
-    println!("cargo:rustc-link-search=/leanstore/shared-headers");
-    println!("cargo:rustc-link-search=/leanstore/cmake-build-debug/vendor");
-    println!("cargo:rustc-link-search=/leanstore/cmake-build-debug/vendor/tabulate/include");
+    // TODO(DB): Get rid of static linking and create a build pipeline
+    println!("cargo:rustc-link-search=leanstore/backend/leanstore");
+    println!("cargo:rustc-link-search=leanstore/shared-headers");
+    println!("cargo:rustc-link-search=leanstore/cmake-build-debug/backend");
+    println!("cargo:rustc-link-search=leanstore/cmake-build-debug/vendor");
+    println!("cargo:rustc-link-search=leanstore/cmake-build-debug/vendor/tabulate/include");
+    println!("cargo:rustc-link-lib=tbb");
+    println!("cargo:rustc-link-lib=aio");
+    println!("cargo:rustc-link-lib=gflags");
+    println!("cargo:rustc-link-lib=stdc++");
 
+    println!("cargo:rustc-link-lib=static=leanstore");
     let bindings = bindgen::Builder::default()
         .header("leanstore/backend/leanstore/c.h")
         .detect_include_paths(true)
-        .clang_arg("-I/leanstore/shared-headers")
-        .clang_arg("-I/leanstore/cmake-build-debug/vendor")
-        .clang_arg("-I/leanstore/cmake-build-debug/vendor/tabulate/include")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .size_t_is_usize(true)
         .generate()
         .expect("Unable to generate bindings");
 
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-use std::ffi::{c_char, CStr};
-use leanquery::{leanstore_init_config, leanstore_open, LeanStoreConfig};
+use std::ffi::{c_char, CStr, CString};
+use leanquery::{leanstore_create_table, leanstore_get_adapter_u8, leanstore_get_cr_manager, leanstore_init_config, leanstore_open, LeanStoreConfig};
 
 fn main() {
     let config = unsafe { leanstore_init_config() };
     let db = unsafe { leanstore_open(config) };
+    let adapter = unsafe { leanstore_get_adapter_u8() };
+    println!("adapter u8: {:?}", adapter);
+    let concurrency_manager = unsafe { leanstore_get_cr_manager(db) };
+    println!("concurrency manager: {:?}", concurrency_manager);
+    let mut table_name = CString::new("Table0").unwrap();
+    let _ = unsafe { leanstore_create_table(concurrency_manager, adapter, db, 0, table_name.into_raw()); };
 }
